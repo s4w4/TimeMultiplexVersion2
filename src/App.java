e3940233ad217e7c8cf67c7ee25d8ad28d1c8002
sawa112 <AlexanderSawadski@gmail.com>
2015-01-19 19:41:29 +0100

    RandomGenerator

---
+++ src/App.java
@@ -0,0 +1,39 @@
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import station.RandomGenerator;
+
+public class App {
+	public static void main(String[] args) throws NoSuchAlgorithmException,
+			InterruptedException {
+		 RandomGenerator random = new RandomGenerator();
+		
+		 System.out.println(random.nextInt());  
+		
+//		try {
+//
+//			// Create a secure random number generator using the SHA1PRNG
+//			// algorithm
+//			SecureRandom secureRandomGenerator = SecureRandom
+//					.getInstance("SHA1PRNG");
+//
+//			// Get 128 random bytes
+//			byte[] randomBytes = new byte[128];
+//			secureRandomGenerator.nextBytes(randomBytes);
+//
+//			// Create two secure number generators with the same seed
+//			int seedByteCount = 5;
+//			byte[] seed = secureRandomGenerator.generateSeed(seedByteCount);
+//
+//			SecureRandom secureRandom1 = SecureRandom.getInstance("SHA1PRNG");
+//			secureRandom1.setSeed(seed);
+//			SecureRandom secureRandom2 = SecureRandom.getInstance("SHA1PRNG");
+//			secureRandom2.setSeed(seed);
+//
+//			for (int i = 0; i < 10000; i++)
+//				System.out.println(secureRandom1.nextDouble());
+//
+//		} catch (NoSuchAlgorithmException e) {
+//		}
+	}
+}
